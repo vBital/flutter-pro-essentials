8a1d4b8ad61d78298804b2538013c23d29e8781e
Diogo C. Pereira <130650392+vBital@users.noreply.github.com>
2023-04-28 08:59:12 +0100

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/flutter_pro_essentials/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_pro_essentials
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pro_essentials/app.dart';
+
+void main() => runApp(App());
